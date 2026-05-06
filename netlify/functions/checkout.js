f0a47a5857007d8184dfd54b81798f49a8a2d08c
tishparkinson <tish4575@gmail.com>
2026-05-05 23:40:18 -0600

    email upon checkout instead of passing it through

--- netlify/functions/checkout.js
+++ netlify/functions/checkout.js
@@ -1,7 +1,4 @@
 // netlify/functions/checkout.js
-// Creates a Stripe Checkout Session and redirects user to it
-// Called when user clicks "Unlock" on /pricing/
-
 import Stripe from "stripe";
 
 export default async (req) => {
@@ -16,7 +13,6 @@ export default async (req) => {
     }],
     success_url: "https://geotipper.com/activate?session_id={CHECKOUT_SESSION_ID}",
     cancel_url: "https://geotipper.com/pricing",
-    customer_email: null, // Stripe will ask for email on the checkout page
   });
 
   return Response.redirect(session.url, 303);
